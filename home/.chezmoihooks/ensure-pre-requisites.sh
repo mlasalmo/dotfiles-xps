33a30fc7cbf920c8c789a8f92890db7e9d61019b
Mariano Lasala <mlasalmo@users.noreply.github.com>
2025-01-19 18:37:29 +0100

    Adapt to use with different devices

--- home/.chezmoihooks/ensure-pre-requisites.sh
+++ home/.chezmoihooks/ensure-pre-requisites.sh
@@ -6,11 +6,24 @@
 set -eu
 
 wanted_packages=(
-  git  # used to find the latest revisions of github repositories
   curl # used to find the latest version of github repositories
-  keepassxc
+  git  # used to find the latest revisions of github repositories
   zsh
 )
+
+OS="$(uname -s)"
+case "${OS}" in
+  Darwin)
+    wanted_packages=(bitwarden-cli)
+    ;;
+  Linux)
+    wanted_packages+=(keepassxc)
+    ;;
+  *)
+    echo "Unsupported OS: ${OS}"
+    exit 1
+    ;;
+esac
 
 missing_packages=()
 
@@ -27,11 +40,24 @@ fi
 # shellcheck source=../.chezmoitemplates/scripts-library
 source "${CHEZMOI_SOURCE_DIR?}/.chezmoitemplates/scripts-library"
 
-log_task "Installing missing packages with APT: ${missing_packages[*]}"
-
-# Chezmoi keepassxc functionality to work with attachments (SSH keys) requires at least version 2.7.0.
-# https://www.chezmoi.io/reference/templates/keepassxc-functions/keepassxcAttachment/
-c sudo add-apt-repository --yes ppa:phoerious/keepassxc
-c sudo apt-get update
-c sudo DEBIAN_FRONTEND=noninteractive apt-get install --yes --no-install-recommends "${missing_packages[@]}"
-c sudo update-ca-certificates
+case "${OS}" in
+  Darwin)
+    log_task "Installing missing packages with Homebrew: ${missing_packages[*]}"
+    c brew install "${missing_packages[@]}"
+    exit 0
+    ;;
+  Linux)
+    log_task "Installing missing packages with APT: ${missing_packages[*]}"
+    # Chezmoi keepassxc functionality to work with attachments (SSH keys) requires at least version 2.7.0.
+    # https://www.chezmoi.io/reference/templates/keepassxc-functions/keepassxcAttachment/
+    c sudo add-apt-repository --yes ppa:phoerious/keepassxc
+    c sudo apt-get update
+    c sudo DEBIAN_FRONTEND=noninteractive apt-get install --yes --no-install-recommends "${missing_packages[@]}"
+    c sudo update-ca-certificates
+    exit 0
+    ;;
+  *)
+    echo "Unsupported OS: ${OS}"
+    exit 1
+    ;;
+esac
